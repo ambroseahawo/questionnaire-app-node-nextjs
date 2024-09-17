26d96dfc17bfa9866422e85a9d1e0cfede49a768
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-09-17 12:39:06 +0300

    Implement Questionnaire model

---
+++ src/models/Questionnaire.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IAnswer {
+  text: string;
+  weight: number;
+  isCorrect: boolean;
+}
+
+interface IQuestion {
+  question: string;
+  answers: IAnswer[];
+}
+
+export interface IQuestionnaire extends Document {
+  title: string;
+  questions: IQuestion[];
+}
+
+const answerSchema = new Schema<IAnswer>({
+  text: { type: String, required: true },
+  weight: { type: Number, required: true },
+  isCorrect: { type: Boolean, required: true },
+});
+
+const questionSchema = new Schema<IQuestion>({
+  question: { type: String, required: true },
+  answers: [answerSchema],
+});
+
+const questionnaireSchema = new Schema<IQuestionnaire>({
+  title: { type: String, required: true },
+  questions: [questionSchema],
+});
+
+const Questionnaire = mongoose.model<IQuestionnaire>('Questionnaire', questionnaireSchema);
+export default Questionnaire;
