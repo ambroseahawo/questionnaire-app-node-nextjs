f2431ed41bc9db4d4dae5d0c77e10bfe7ff52538
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-09-17 13:09:03 +0300

    Implement get all questionnaires route

--- src/routes/questionnaire.ts
+++ src/routes/questionnaire.ts
@@ -12,7 +12,17 @@ router.post('/create', async (req: Request, res: Response) => {
     const newQuestionnaire = new Questionnaire({ title, questions });
     const savedQuestionnaire = await newQuestionnaire.save();
     res.status(201).json(savedQuestionnaire);
-  } catch (err) {
-    res.status(500).json({ error: (err as Error).message });
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
+
+// get all questionnaires
+router.get('/all', async (req: Request, res: Response) => {
+  try {
+    const questionnaires = await Questionnaire.find();
+    res.status(200).json(questionnaires);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
   }
 });
